72a7cf53514df428a53fe89f0711d1bf19b5733d
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2023-09-14 19:14:31 +0200

    b

--- frågesport/Program.cs
+++ frågesport/Program.cs
@@ -2,22 +2,26 @@
 int questionNum = 0;
 string answer;
 
+//Creates an array of questions with their answers. Makes it much easier to use a question and to check if the player's answer is correct
+//Also makes it a lot easier to add a question
 string[][] questions = {
     new string[] {"\nWhat is Wiggo's name \na) mario \nb) Wiggo \nc) wigo \nd) pytor dragetovich idi nahui", "b"},
     new string[] {"\n2+2? \na) 1 \nb) 2 \nc) 8 \nd) 4", "d"},
-    new string[] {"\näta bajs? \na) nej \nb) på måndagar \nc) ja \nd) hej", "b"},
+    new string[] {"\ngo o lägg dig bror \na) nej \nb) bara på måndagar \nc) ja \nd) hej", "b"},
     new string[] {"\nhur kommer man till odenplan?? \na)gå hem \nb) man gör det inte \nc) det är omöjligt \nd) fråga nån annan", "c"}
 };
 
 Console.WriteLine("Svara genom att skriva a, b, c eller d");
 Console.ReadLine();
 
+//The actual quiz game. Uses an integer to select the question and the answer it should be checking for (question & correct)
 void askQuestion(string question, string correct){
     answer = null;
 
+    //Asks the question and reads the player's answer. Repeats if the player's input isn't a, b, c or d
     do {
     Console.WriteLine($"\nFråga {questionNum + 1}. {question}");
-    answer = Console.ReadLine().Trim();
+    answer = Console.ReadLine().Trim().ToLower(); //removes whitespaces before and after the actual answer
     } while (answer != "a" && answer != "b" && answer != "c" && answer != "d");
 
     if (answer == correct){
@@ -30,6 +34,7 @@ void askQuestion(string question, string correct){
 
     questionNum += 1;
 
+    //Ends the game if questionNum is bigger than the amount of questions
     if (questionNum >= questions.Count()){
         Console.WriteLine($"\nok får se om du är bra eller inte... \n\n{points} poäng!");
 
@@ -48,11 +53,13 @@ void askQuestion(string question, string correct){
 
     }
     else {
+        //Continues if it isn't
         askQuestion(questions[questionNum][0], questions[questionNum][1]);
     }
 }
 
-askQuestion(questions[questionNum][0], questions[questionNum][1]);
+//Asks the first question
+askQuestion(questions[0][0], questions[0][1]);
 
 
 Console.ReadLine();
